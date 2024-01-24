3da25cb08ad1a4b1b62e6a5a5702d0a29c43d79e
Wagner Rodrigues <wagner92dev@gmail.com>
2024-01-24 20:35:04 -0300

    Criando as classes Service e Client

---
+++ src/main/java/com/portal/api/service/CarPostStoreServiceImpl.java
@@ -0,0 +1,28 @@
+package com.portal.api.service;
+
+import com.portal.api.client.CarPostStoreClient;
+import com.portal.api.dto.CarPostDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CarPostStoreServiceImpl implements CarPostStoreService{
+
+    @Autowired
+    private CarPostStoreClient carPostStoreClient;
+
+    @Override
+    public List<CarPostDTO> getCarForSales() {
+        return carPostStoreClient.carForSaleClient();
+    }
+
+    @Override
+    public void changeCarForSale(CarPostDTO carPost, String id) {
+        carPostStoreClient.changeCarForSaleClient(carPost, id);
+    }
+
+    @Override
+    public void removeCarForSale(String id) {
+        carPostStoreClient.deleteCarForSaleClient(id);
+    }
+}
